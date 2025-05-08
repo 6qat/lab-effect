28992e2e1750da29c6047a0788b16da7415b3217
Guilherme C. B. Moreira <ceschiatti@gmail.com>
2025-05-08 10:17:15 -0300

    Continue playing with Schemas

--- src/schema.ts
+++ src/schema.ts
@@ -6,10 +6,57 @@ const MySimpleSchema = Schema.Struct({
   isActive: Schema.Boolean,
 });
 
-const _i = Schema.decode(MySimpleSchema)({
+const decoded = Schema.decode(MySimpleSchema)({
   id: "1",
   title: "Hello",
   isActive: true,
 });
 
-console.log(_i);
+console.log(decoded);
+
+const decodedUnknown = Schema.decodeUnknown(MySimpleSchema)({
+  id: "1",
+  title: "Hello",
+  isActive: true,
+});
+
+console.log(decodedUnknown);
+
+const decodedUnknownEither = Schema.decodeUnknownEither(MySimpleSchema)({
+  id: "1",
+  title: "Hello",
+  //   isActive: true,
+});
+
+if (Either.isRight(decodedUnknownEither)) {
+  console.log(decodedUnknownEither.right);
+} else {
+  console.log(decodedUnknownEither.left.message);
+}
+
+const NumberFromString = Schema.NumberFromString;
+const _number = Schema.decode(NumberFromString)("123"); // 123 (number)
+
+// ================================================================
+
+const MySimpleSchema2 = Schema.Struct({
+  id: Schema.String,
+  //   title: Schema.Union(Schema.String, Schema.Null),
+  //   title: Schema.NullOr(Schema.String),
+  //   title: Schema.UndefinedOr(Schema.String),
+  //   title: Schema.optional(Schema.String),
+  title: Schema.String.pipe(Schema.filter((s) => s.trim() === s)),
+  isActive: Schema.Boolean,
+});
+
+const decodedUnknownEither2 = Schema.decodeUnknownEither(MySimpleSchema2)({
+  id: "1",
+  //   title: null,
+  isActive: true,
+});
+
+if (Either.isRight(decodedUnknownEither2)) {
+  console.log(decodedUnknownEither2.right);
+} else {
+  console.log(decodedUnknownEither2.left.message);
+}
